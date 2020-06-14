da5707b4d571f1e5faf3566d03a6c32166016b6f
Marinos <anagnmar@hotmail.com>
2020-06-14 23:28:20 +0300

    Initial_Setup

---
+++ QuirkyBookRental/Models/Book.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace QuirkyBookRental.Models
+{
+    public class Book
+    {
+
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string ISBN { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Author { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ImageUrl { get; set; }
+
+        [Required]
+        [Range(0, 1000)]
+        public int Availability { get; set; }
+
+
+        [DataType(DataType.Currency)]
+        public double Price { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: dd MMM yyyy}")]
+        public DateTime DateAdded { get; set; }
+
+        // Navigation  property
+        public Genre Genre { get; set; }
+
+        [Required]
+        public int GenreId { get; set; }
+        //
+
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0: dd MMM yyyy}")]
+        public DateTime PublicationDate { get; set; }
+
+        [Required]
+        public int Pages { get; set; }
+
+        [Required]
+        public string ProductDimensions { get; set; }
+
+    }
+}
